fad147609f84d8257683bce6dea91c431c147d48
LecomteAdrien <lecomteadrien@hotmail.fr>
2021-03-23 11:40:42 +0100

    Step 1 OK

--- src/test/java/org/sixdouglas/formation/spring/irrigation/DripIrrigationTest.java
+++ src/test/java/org/sixdouglas/formation/spring/irrigation/DripIrrigationTest.java
@@ -28,7 +28,7 @@ class DripIrrigationTest {
 
         Flux<Drop> dropFlux = dripIrrigation.followDrops()
                 .limitRequest(5)
-                .timeout(Duration.ofMillis(105));
+                .timeout(Duration.ofMillis(500));
 
         StepVerifier.create(dropFlux).assertNext(drop -> {
             assertEquals(1, drop.getGreenHouseId(), "Greenhouse ID should be 1");
